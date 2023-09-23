840806951f3df9439ab124c70fd3bd770e350e8b
NickGutierrezFarmingdale <gutinr@farmingdale.edu>
2023-09-23 16:36:15 -0400

    Fixed Procfile

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,16 +4,6 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		client.on('messageCreate', (message) => {
-			// Ignore messages sent by the bot itself
-			if (message.author.bot) return;
-		  
-			if (message.content === '!hello') {
-			  message.reply('Hello, world!');
-			}
-		  });
-
-
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
